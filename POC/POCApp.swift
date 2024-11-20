da7dc96e80f0dab37741bede434fbcf833fc6c53
Gianfranco Fois <gianfois451@gmail.com>
2024-11-20 15:58:11 -0300

    Initial Commit

---
+++ POC/POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCApp.swift
+//  POC
+//
+//  Created by Gian Fois on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
